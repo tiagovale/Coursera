4b9f6f83405cd5595aa2b9a94fd8a1972d67e757
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2015-12-27 10:07:06 -0200

    Finished the task 3, displaying the comment live preview only if the form is valid.

--- Full_Stack_Web_Development/ANGULARJS/week2/conFusion/app/scripts/app.js
+++ Full_Stack_Web_Development/ANGULARJS/week2/conFusion/app/scripts/app.js
@@ -157,18 +157,19 @@ angular.module('confusionApp', [])
             //Step 1: Create a JavaScript object to hold the comment from the form
             $scope.comment={author:"",rating:5,comment:"",date:""};
 		
-          /*   $scope.submitComment = function () {
+             $scope.submitComment = function () {
                 
                 //Step 2: This is how you record the date
-                "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+                $scope.comment.date = new Date().toISOString();
                 
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push($scope.comment);
                 
                 //Step 4: reset your form to pristine
-                
+                $scope.commentForm.$setPristine();
                 //Step 5: reset your JavaScript object that holds your comment
-            }*/
+				 $scope.comment={author:"",rating:5,comment:"",date:""};
+            } 
         }]) ;
 
 
