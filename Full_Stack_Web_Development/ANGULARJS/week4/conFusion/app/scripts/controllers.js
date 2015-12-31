84583687e0b9c3137250405c4fa0a2de7cd69d35
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2015-12-31 20:20:25 -0200

    Fetching the data of the menu and index page through the json-server instead of services.js.

--- Full_Stack_Web_Development/ANGULARJS/week4/conFusion/app/scripts/controllers.js
+++ Full_Stack_Web_Development/ANGULARJS/week4/conFusion/app/scripts/controllers.js
@@ -8,7 +8,12 @@ angular.module('confusionApp')
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes = menuFactory.getDishes();
+    $scope.dishes ={};
+	menuFactory.getDishes().then(
+	function(response){
+	$scope.dishes=response.data;
+	}
+	);
 
 
     $scope.select = function(setTab) {
@@ -84,9 +89,15 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-    var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
+    
 
-    $scope.dish = dish;
+    $scope.dish = {};
+	menuFactory.getDish(parseInt($stateParams.id, 10)).then(
+	function(response){
+	$scope.dish=response.data;
+	}
+	);
+	
 
 }])
 
@@ -121,7 +132,12 @@ angular.module('confusionApp')
 
 .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
 
-    $scope.dish = menuFactory.getDish(0);
+    $scope.dish = {};
+	menuFactory.getDish(0).then(
+	function(response){
+	$scope.dish=response.data;
+	} 
+	);
     $scope.promotion = menuFactory.getPromotion(0);
     $scope.leader = corporateFactory.getLeader(3);
 
