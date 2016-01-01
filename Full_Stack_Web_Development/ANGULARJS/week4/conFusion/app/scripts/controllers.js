7f2a4ea67f24ef3c6126840e7a6a709a6dafe7e1
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-01-01 12:34:21 -0200

    Handled exceptions using angular resource module.

--- Full_Stack_Web_Development/ANGULARJS/week4/conFusion/app/scripts/controllers.js
+++ Full_Stack_Web_Development/ANGULARJS/week4/conFusion/app/scripts/controllers.js
@@ -7,9 +7,16 @@ angular.module('confusionApp')
     $scope.tab = 1;
     $scope.filtText = '';
     $scope.showDetails = false;
-	$scope.showMenu = true;
+	$scope.showMenu = false;
 	$scope.message = "Loading ...";
-	$scope.dishes = menuFactory.getDishes().query();
+	$scope.dishes = menuFactory.getDishes().query(
+	function (response){
+	$scope.dishes=response;
+	$scope.showMenu=true;
+	},
+	function (response){
+	$scope.message = "Error: " + response.status + " " + response.statusText;
+	});
 
 
     $scope.select = function(setTab) {
@@ -86,11 +93,18 @@ angular.module('confusionApp')
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
     
-	 $scope.showDish = true;
+	 $scope.showDish = false;
    	 $scope.message="Loading ...";
-	 $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+	 $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+	.$promise.then(
+			function (response){
+			$scope.dishes=response;
+			$scope.showMenu=true;
+			},
+			function (response){
+			$scope.message = "Error: " + response.status + " " + response.statusText;
+			});
 	
-
 }])
 
 .controller('DishCommentController', ['$scope', function($scope) {
@@ -125,9 +139,17 @@ angular.module('confusionApp')
 .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
 
     
-	$scope.showDish = true;
+	$scope.showDish = false;
 	$scope.message="Loading ...";
-	$scope.dish = menuFactory.getDishes().get({id:0});
+	$scope.dish = menuFactory.getDishes().get({id:0})
+	.$promise.then(
+			function (response){
+			$scope.dishes=response;
+			$scope.showMenu=true;
+			},
+			function (response){
+			$scope.message = "Error: " + response.status + " " + response.statusText;
+			});
     $scope.promotion = menuFactory.getPromotion(0);
     $scope.leader = corporateFactory.getLeader(3);
 
